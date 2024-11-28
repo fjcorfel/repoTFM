27bcca79a7bf3d1028017a7186e3bde1273b7447
Fran Cordero <159131020+fjcorfel@users.noreply.github.com>
2024-11-28 15:43:28 +0100

    Update treetime_ancestral_parallel.sh

--- treetime_ancestral_parallel.sh
+++ treetime_ancestral_parallel.sh
@@ -3,4 +3,4 @@
 # Usar nproc para comprobar el número de núcleos disponibles (para -j)
 # Ejecutar en la carpeta split_results/
 
-parallel -j 16 treetime ancestral --aln {} --tree ../../data/global_rescatado_RELATIVE.nwk --outdir ancestral_results/{.} ::: *.fas
+parallel -j 3 treetime ancestral --aln {} --tree ../../data/global_rescatado_RELATIVE_CLEANED.nwk --outdir ancestral_results/{.} ::: *.fas
