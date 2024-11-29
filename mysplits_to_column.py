7691675f2d57e255d23b7d4f3a69fa0857dd7cf0
fjcorfel <fjcorfel@gmail.com>
2024-11-29 22:10:53 +0100

    Organize this mess

--- mysplits_to_column.py
+++
@@ -1,9 +0,0 @@
-with open('data/mysplits.txt') as file:
-    line = file.readline()
-
-numbers = line.strip()[1:-1].split(', ')
-
-with open('mysplits_column.txt', 'w') as output_file:
-    for number in numbers:
-        output_file.write(f"{number.strip()}\n")
-        
--- parseMutations.R
+++ parseMutations.R
@@ -2,10 +2,22 @@ library(treeio)
 library(ape)
 library(dplyr)
 
-# Read splits
-splits <- as.integer(readLines("data/mysplits_column.txt")) 
+# Read SNP table -> df
+snpTable <- read.table("./data/SNP_table_noresis.txt", sep = "\t", header = TRUE)
+
+# Read splits -> vector
+splits <- readLines("./data/mysplits.txt")
+splits <- gsub("\\[|\\]", "", splits)
+splits <- as.numeric(unlist(strsplit(splits, ", ")))
+
+
+
+# For each element of the split, open a tree
+
+
+
 
 # Read tree from file -> phylo object
-tree <- read.beast(file = "annotated_trees/annotated_tree_001.nexus")
+#tree <- read.beast(file = "annotated_trees/annotated_tree_001.nexus")
 # Tree tibble conversion
-tibble_tree <- as_tibble(tree)
+#tibble_tree <- as_tibble(tree)
