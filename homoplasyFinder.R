f832a43bcfa96b599522b956d822a327de4f294e
fjcorfel <fjcorfel@gmail.com>
2025-01-21 07:25:00 +0100

    Refactor homoplasyFinder.R: Enhance input loading and processing with parallelization and helper functions

--- homoplasyFinder.R
+++ homoplasyFinder.R
@@ -3,103 +3,126 @@ library(phangorn)
 library(dplyr)
 library(data.table)
 library(phytools)
+library(parallel)
 
-### Load inputs ###
 
-# Tree
+### LOAD INPUTS ###
+
 print("Loading tree...")
 tree <- ape::as.phylo(treeio::read.beast("../data/annotated_tree.nexus")) 
-# SNP table
+
 print("Loading SNP table...")
 snp_table <- data.table::fread("../data/SNP_table_noresis.txt") %>%
   select(Position, WT, ALT)
+
 print("Loading mutations table (ancestral recosntruction)...")
-# result_tree (mutations per node table)
 load("../data/ancestral_result.rda")
 
 
-### Processing ###
 
-# List for saving nodes associated to each SNP mutation
-node_mutations_list <- list()
+### HELPER FUNCTIONS ###
 
-# Iterate for each position of SNP table
-for (n_position in seq_along(snp_table$Position)) {
+# Get mutations from a given node
+get_node_mutations <- function(result_tree, node_number) {
+  mutations <- unlist(result_tree[node_number, "ref_mutation_position"])
+  if (is.null(mutations) || length(mutations) == 0) return(NULL)
+  return(mutations)
+}
+
+# Check if mutation exists in sister AND parent nodes
+mutation_in_sister_parent <- function(tree, result_tree, node_number, mutation) {
+  sister_node_number <- phytools::getSisters(tree, node_number)
+  sister_mutations <- get_node_mutations(result_tree, sister_node_number)
   
-  # Extract mutation
-  position <- snp_table$Position[n_position]
+  parent_node_number <- phytools::getParent(tree, node_number)
+  parent_mutations <- get_node_mutations(result_tree, parent_node_number)
+  
+  return(mutation %in% c(sister_mutations, parent_mutations))
+}
+
+#? Check if descendants acquired homoplasy (check reversions)
+mutation_in_descendants <- function(tree, result_tree, node_number, mutation) {
+  descendants <- unlist(phangorn::Descendants(tree, node_number, "all"))
+  if (is.null(descendants) || length(descendants) == 0) return (FALSE)
+  
+  any(sapply(descendants, function(descendant) {
+    mutations <- unlist(result_tree[descendant, "ref_mutation_position"])
+    !is.null(mutations) && mutation %in% mutations
+  }))
+  
+  #! TODO: habría que añadir FLAG a result_tree
+}
+
+# Count alleles for mutation and WT in tips
+count_tip_alleles <- function(tree, result_tree, node_number, mutation) {
+  tips <- unlist(phangorn::Descendants(tree, node_number, "tips"))
+  
+  # We need at least 2 tips with each allele
+  if (length(tips) < 4) return(NULL)
+  
+  mut_alleles <- sum(sapply(tips, function(tip) mutation %in% get_node_mutations(result_tree, tip)))
+  wt_alleles <- length(tips) - mut_alleles
+  
+  return(list(mut_alleles = mut_alleles, wt_alleles = wt_alleles))
+}
+
+# Process a SNP position from SNP table across all nodes
+# Worker function for parallelization
+find_homoplasy <- function(n_position, snp_table, tree, result_tree) {
+  
+  # List for saving homoplasy nodes that meet the criteria
+  # 1. No descendant nodes that acquired homoplasy
+  # 2. At least 2 descendant tips of each allele
+  homoplasy_nodes <- list()
+  
+  # Get SNP mutation
+  snp_position <- snp_table$Position[n_position]
   wt <- snp_table$WT[n_position]
   alt <- snp_table$ALT[n_position]
-  snp_mutation <- paste0(wt, position, alt)
+  snp_mutation <- paste0(wt, snp_position, alt)
   
-  # Iterate for each node of ancestral result
+
   for (n_node in seq_along(result_tree$node)) {
+    # Check if node has the snp_mutation
+    node_mutations <- get_node_mutations(result_tree, n_node)
+    if (is.null(node_mutations) || !snp_mutation %in% node_mutations) next
     
-    # Get mutations of the node
-    ref_mutations <- unlist(result_tree[n_node, "ref_mutation_position"])
-    if (is.null(ref_mutations) || length(ref_mutations) == 0) next
+    # Check sister and parent nodes
+    if (mutation_in_sister_parent(tree, result_tree, n_node, snp_mutation)) next
     
-    # Iterate over mutations of the node
-    for (node_mutation in ref_mutations) {
-      
-      # If node has the SNP mutation, we save it
-      if (node_mutation == snp_mutation) {
-        node_label <- result_tree[n_node, ]$label
-        node_info <- list(
-          node_number = n_node,
-          node_label = node_label,
-          node_mutation = snp_mutation
-        )
-        node_mutations_list <- append(node_mutations_list, list(node_info))
-        
-      }
-    }
-  }
-}
-
-### Filtering ###
-
-# List of nodes that meet the filtering criteria
-homoplasy_nodes <- list()
-
-# Iterate over saved nodes for filtering
-for (n_node in seq_along(node_mutations_list)) {
-  
-  node_info <- node_mutations_list[[n_node]]
-  
-  # Check if sister or parent has mutation
-  sister_node_number <- phytools::getSisters(tree, node_info$node_number)
-  sister_node_mutations <- unlist(result_tree[sister_node_number, "ref_mutation_position"])
-  
-  parent_node_number <- phytools::getParent(tree, node_info$node_number)
-  parent_node_mutations <- unlist(result_tree[parent_node_number, "ref_mutation_position"])
-  
-  if (mutation %in% sister_node_mutations || mutation %in% parent_node_mutations) next # Skip this node
-  
-  # Check if node has at least 2 tips for each allele
-  tips <- unlist(phangorn::Descendants(tree, node_info$node_number, "tips"))
-  
-  if (length(tips) < 4) next # Skip this node
-  
-  mut_alleles <- 0
-  wt_alleles <- 0
-  for (tip in tips) {
-    tip_mutations <- unlist(result_tree[tip, "ref_mutation_position"])
+    # Check alleles in tips
+    tip_allele_counts <- count_tip_alleles(tree, result_tree, n_node, snp_mutation)
+    if (is.null(tip_allele_counts)) next
     
-    if (mutation %in% tip_mutations) {
-      mut_alleles <- mut_alleles + 1
-    } else {
-      wt_alleles <- wt_alleles + 1
-    }
+    # If node meets all the criteria -> save node
+    node_info <- list(
+      node_number = n_node,
+      node_label = result_tree[n_node, "label"],
+      node_mutation = snp_mutation,
+      mut_alleles = tip_allele_counts$mut_alleles,
+      wt_alleles = tip_allele_counts$wt_alleles
+    )
+    homoplasy_nodes <- append(homoplasy_nodes, list(node_info))
   }
   
-  # We save the node and its tips info if it meets the criteria
-  node_info[["mut_alleles"]] <- mut_alleles
-  node_info[["wt_alleles"]] <- wt_alleles
-  
-  # NODOS CON MUTACIONES QUE SUPEREN EL FILTRADO SE MARCARÁN COMO HOMOPLASIAS
-  homoplasy_nodes <- append(homoplasy_nodes, list(node_info))
+  return(homoplasy_nodes)
 }
 
 
+
+### MAIN PROCESSING ###
+
+print("Starting parallel processing...")
+n_cores <- detectCores() 
+
+homoplasy_nodes <- mclapply(seq_along(snp_table$Position), function(n_position) {
   
+  find_homoplasy(n_position, snp_table, tree, result_tree)
+  
+}, mc.cores = n_cores)
+
+# Combine lists returned by each worker function into a single list
+homoplasy_nodes <- do.call(c, homoplasy_nodes)
+
+
+print("Processing complete.")
