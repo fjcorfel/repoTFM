eefe92f8704ca0839d55dccc1451b0a5212403ee
fjcorfel <fjcorfel@gmail.com>
2025-01-27 12:48:55 +0100

    Refactor filterHomoplasySNPtable.R: Save SNP mutation counts instead of binary table

--- filterHomoplasySNPtable.R
+++ filterHomoplasySNPtable.R
@@ -6,7 +6,6 @@ library(parallel)
 snp_table <- fread("../data/SNP_table_noresis_mutations.txt")
 # Vector of all SNPs
 snps <- snp_table$Mutation
-
 rm(snp_table)
 
 # Load mutation table (ancestral_result -> result_tree)
@@ -15,7 +14,6 @@ result_tree <- result_tree %>%
   select(node, label, ref_mutation_position)
 # List with mutations for each node
 muts <- result_tree$ref_mutation_position
-muts <- sample(muts, size = 1000)
 rm(result_tree)
 
 # Create binary table (rows = nodes, cols = SNPs)
@@ -28,9 +26,8 @@ binary_table <- mclapply(muts, function(node_muts) {
 
 binary_table <- as.data.frame(do.call(rbind, binary_table))
 
-
 # Sum of the total of nodes containing each mutation
 snp_count <- colSums(binary_table)
 names(snp_count) <- names(snps)
-save(binary_table, file = "../data/SNP_mutation_count.rda")
+save(snp_count, file = "../data/SNP_mutation_count.rda")
 
