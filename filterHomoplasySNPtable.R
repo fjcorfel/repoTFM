8eb7d4fef90f077ddff9150b40061dd92aa23d33
fjcorfel <fjcorfel@gmail.com>
2025-01-27 17:34:35 +0100

    Refactor filterHomoplasySNPtable.R: Optimize binary table creation using parallel clustering processing

--- filterHomoplasySNPtable.R
+++ filterHomoplasySNPtable.R
@@ -6,6 +6,7 @@ library(parallel)
 snp_table <- fread("../data/SNP_table_noresis_mutations.txt")
 # Vector of all SNPs
 snps <- snp_table$Mutation
+snps <- head(snps, n = 1000)
 rm(snp_table)
 
 # Load mutation table (ancestral_result -> result_tree)
@@ -14,20 +15,28 @@ result_tree <- result_tree %>%
   select(node, label, ref_mutation_position)
 # List with mutations for each node
 muts <- result_tree$ref_mutation_position
+muts <- head(muts, n = 1000)
 rm(result_tree)
 
+# Set up cluster
+n_cores <- 14
+cl <- makeCluster(n_cores)
+
+# Share objects to the cluster
+clusterExport(cl, varlist = c("snps"))
+
 # Create binary table (rows = nodes, cols = SNPs)
-# 
-binary_table <- mclapply(muts, function(node_muts) {
-
-  # Check if SNP is in node mutations
+binary_table <- parLapply(cl, muts, function(node_muts){
   as.numeric(snps %in% node_muts)
-}, mc.cores = 14)
+})
 
+# Stop the cluster
+stopCluster(cl)
+
+# Combine results into a single data frame
 binary_table <- as.data.frame(do.call(rbind, binary_table))
 
 # Sum of the total of nodes containing each mutation
 snp_count <- colSums(binary_table)
-names(snp_count) <- names(snps)
+names(snp_count) <- snps
 save(snp_count, file = "../data/SNP_mutation_count.rda")
-
