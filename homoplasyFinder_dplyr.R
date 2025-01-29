b11c84581f57498db00a52799bb8724ff6d093bd
fjcorfel <fjcorfel@gmail.com>
2025-01-29 11:40:20 +0100

    Add functions to check for reversions and mutations in sister branches in homoplasyFinder_dplyr.R

--- homoplasyFinder_dplyr.R
+++ homoplasyFinder_dplyr.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(data.table)
 library(phytools)
 library(parallel)
+library(stringr)
 
 
 ### LOAD INPUTS ###
@@ -51,6 +52,23 @@ get_wt_alleles <- function(node) {
   return(length(sister_tips))
 }
 
+check_reversions <- function(node, mutation) {
+  mutation_position <- as.numeric(str_extract(mutation, "\\d+"))
+  descendants <- phangorn::Descendants(tree, node, "all")
+  descendant_mutations <- sapply(descendants, get_node_mutations)
+  
+  matches <- str_detect(descendant_mutations, paste0("(?<=\\D)", mutation_position, "(?=\\D)"))
+  return(any(matches))
+}
+
+check_mutations_in_wt <- function(node, mutation) {
+  sister <- phytools::getSisters(tree, node)
+  sister_descendants <- unlist(phangorn::Descendants(tree, sister, "all"))
+  sister_descendant_mutations <- unlist(sapply(sister_descendants, get_node_mutations))
+  
+  return(mutation %in% sister_descendant_mutations)
+}
+
 
 # Find if given SNP table position contains homoplasy
 find_homoplasy <- function(n_position) {
@@ -59,7 +77,7 @@ find_homoplasy <- function(n_position) {
   snp_mutation <- snps[n_position]
 
   # Debugging
-  if(n_position %% 1000 == 0) {
+  if(n_position %% 1 == 0) {
     print(paste0(format(Sys.time(), "%Y-%m-%d %H:%M:%S"),
                  " - Position: ", n_position,
                  " - Mutation: ", snp_mutation))
@@ -72,12 +90,26 @@ find_homoplasy <- function(n_position) {
     select(parent, node, label, ref_mutation_position) %>%
     ungroup()
   
-  # TODO: Check reversions
+  
   # Criteria for saving a node as homoplasy:
+  # WT and mutation branches are pure (no appearing of the mutation or reversions)
   # At least 2 descendant tips of each allele (mutation and WT)
   
+  # Check if mutation and WT branches are pure
+  filtered_alleles <- nodes_with_mutation %>%
+    rowwise() %>%
+    mutate(
+      has_reversion = check_reversions(node, snp_mutation),
+      has_mutation_in_wt = check_mutations_in_wt(node, snp_mutation)
+    ) %>%
+    filter(
+      !has_reversion, 
+      !has_mutation_in_wt
+    ) %>%
+    ungroup()
+  
   # Filter nodes with 2 tips of each allele
-  nodes_with_alleles <- nodes_with_mutation %>%
+  nodes_with_alleles <- filtered_alleles %>%
     rowwise() %>%
     mutate(
       n_mut_alleles = get_mut_alleles(node)
@@ -109,7 +141,7 @@ n_cores <- detectCores()
 
 homoplasy_nodes <- mclapply(seq_along(snps), function(n_position) {
   find_homoplasy(n_position)
-}, mc.cores = 12)
+}, mc.cores = 14)
 
 # Combine df returned by each worker function into a single df
 homoplasy_nodes <- do.call(rbind, homoplasy_nodes)
