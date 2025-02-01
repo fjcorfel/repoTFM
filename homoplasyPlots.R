3235d56c2a56c5d15fd6b6eb1d6f1df191f58b0e
fjcorfel <fjcorfel@gmail.com>
2025-02-01 12:44:42 +0100

    Enhance homoplasyPlots.R by filtering for multiple mutations and improving visualizations with color scales

--- homoplasyPlots.R
+++ homoplasyPlots.R
@@ -1,36 +1,35 @@
 library(ggplot2)
 library(dplyr)
+library(viridis)
 
 load("../data/homoplasy_mutations.rda")
 
-phor_mutations <- homoplasy_nodes_annotated_byMutation %>%
-  filter(synonym == "phoR")
+filtered_mutations <- homoplasy_nodes_annotated_byMutation %>%
+  filter(synonym %in% c("phoR", "katG", "rpoB", "sigA"))
 
-katg_mutations <- homoplasy_nodes_annotated_byMutation %>%
-  filter(synonym == "katG")
-
-rpob_mutations <- homoplasy_nodes_annotated_byMutation %>%
-  filter(synonym == "rpoB")
-
-siga_mutations <- homoplasy_nodes_annotated_byMutation %>%
-  filter(synonym == "sigA")
 
 # Violin plot
 homoplasy_nodes_annotated_byMutation %>%
   ggplot(aes(x="", y=log2(RoHO))) +
   geom_violin(fill = "coral2") +
-  geom_point(data = phor_mutations, color = "darkslateblue", alpha = 0.8, size = 2.5) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 0.5, alpha = 0.6)
+  geom_jitter(data = filtered_mutations, aes(color = synonym), alpha = 0.8, size = 2) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 0.5, alpha = 0.6) +
+  scale_color_viridis(discrete = TRUE)
+  labs(color = "Gene")
   
  # Histogram
 homoplasy_nodes_annotated_byMutation %>%
   ggplot(aes(x = log2(RoHO))) +
-  geom_histogram(binwidth = 0.1, fill="coral2", color="#e9ecef", alpha = 0.9)
+  geom_histogram(binwidth = 0.25, fill="coral2", color="#e9ecef", alpha = 0.9)
 
 # Boxplot
 homoplasy_nodes_annotated_byMutation %>%
   ggplot(aes(x="",y = log2(RoHO))) +
   geom_boxplot(fill = "coral2") +
-  geom_jitter(data = phor_mutations, color = "darkslateblue", alpha = 0.8, size = 2.5) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 0.5, alpha = 0.6)
+  geom_jitter(data = filtered_mutations, aes(color = synonym), alpha = 0.8, size = 2) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 0.5, alpha = 0.6) +
+  scale_color_viridis(discrete = TRUE) +
+  labs(color = "Gene")
+  
+  
   
