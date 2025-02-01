e793493c6699821cbe8097f15aab15f4f2d92ea2
fjcorfel <fjcorfel@gmail.com>
2025-02-01 12:16:37 +0100

    Add normalization of RoHO values based on gene lengths in normalizeRoHO.R

---
+++ normalizeRoHO.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(data.table)
+
+# Get gene lengths
+snp_table <- fread("../data/SNP_table.txt")
+
+gene_lengths <- snp_table %>%
+  distinct(Rv_number, .keep_all = TRUE) %>%
+  mutate(gene_length = Gene_end - Gene_start + 1) %>%
+  select(Rv_number, gene_length)
+  
+
+# Calculate normalized RoHO value based on gene length
+load("../data/homoplasy_mutations.rda")
+
+homoplasy_nodes_annotated_byMutation_norm <- homoplasy_nodes_annotated_byMutation %>%
+  mutate(
+    gene_length = gene_lengths$gene_length[match(Rv_number, gene_lengths$Rv_number)]
+  ) %>%
+  mutate(
+    normalized_RoHO = RoHO / gene_length
+  )
+
+save(homoplasy_nodes_annotated_byMutation_norm, file = "../data/homoplasy_mutations_norm.rda")
