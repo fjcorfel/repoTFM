8a002030c72c50531a9bf337edaae1962787e047
fjcorfel <fjcorfel@gmail.com>
2025-02-17 17:32:47 +0100

    Refactor tests.R to improve mutation ranking and p-value adjustments; save results to .rda files.

--- tests.R
+++ tests.R
@@ -5,7 +5,7 @@ load("../data/homoplasy_mutations.rda")
 load("../data/homoplasy_nodes.rda")
 snp_table <- fread("../data/SNP_table.txt")
 
-### OLD STRAT ###
+### Mutation Ranking ###
 
 ordered_df <- homoplasy_nodes_annotated_byMutation %>%
   select(mutation, synonym ,Rv_number, RoHO) %>%
@@ -24,12 +24,20 @@ wilcox_results <- sapply(genes, function(gene) {
   return(test$p.value)
 })
 
-wilcox_results_df <- data.frame(Mutation = mutations,
+ranking_results <- data.frame(Mutation = mutations,
                                 Synonym = synonyms,
                                 Rv_number = genes,
                                 pvalue = wilcox_results)
 
-### NEW STRAT ###
+ranking_results$adj_pvalue_Bonf <- p.adjust(ranking_results$pvalue,
+                                            method = "bonferroni")
+ranking_results$adj_pvalue_BH <- p.adjust(ranking_results$pvalue,
+                                          method = "BH")
+
+save(ranking_results, file = "ranking_results.rda")
+
+
+### Paper Strategy ###
 
 tt_results <- homoplasy_nodes_annotated_byMutation %>%
   group_by(Rv_number) %>%
@@ -78,35 +86,25 @@ wilcox_results <- homoplasy_nodes %>%
   summarise(
     n_nodes = n(),
     pvalue = if (n_nodes >= 2) {
-      tryCatch({
         wilcox.test(n_mut_alleles, n_wt_alleles, paired = TRUE)$p.value
-      }, warning = function(w) NA)
     } else NA
   )
 
-tt_results$adj_pvalue <- p.adjust(tt_results$pvalue, method = "bonferroni")
-wilcox_results$adj_pvalue <- p.adjust(wilcox_results$pvalue, method = "bonferroni")
+tt_results$adj_pvalue_Bonf <- p.adjust(tt_results$pvalue, method = "bonferroni")
+tt_results$adj_pvalue_BH <- p.adjust(tt_results$pvalue, method = "BH")
+tt_results <- tt_results %>%
+  rename(tt_pvalue = pvalue,
+         tt_adj_pvalue_Bonf = adj_pvalue_Bonf,
+         tt_adj_pvalue_BH = adj_pvalue_BH)
 
 
-### RoHO - 1 STRAT ### 
-homoplasy_mutations <- homoplasy_nodes_annotated_byMutation %>%
-  mutate(norm_RoHO = RoHO - 1)
 
-tt_results <- homoplasy_mutations %>%
-  group_by(Rv_number) %>%
-  summarise(
-    n_mutations = n(),
-    pvalue = if (n_mutations >= 2) {
-      tryCatch({
-        t.test(norm_RoHO, mu = 0)$p.value
-      }, error = function(e) NA)
-    } else NA
-  ) %>%
-  mutate(
-    synonym = snp_table$Synonym[match(Rv_number, snp_table$Rv_number)]
-  ) %>%
-  select(synonym, Rv_number, n_mutations, pvalue)
+wilcox_results$adj_pvalue_Bonf <- p.adjust(wilcox_results$pvalue, method = "bonferroni")
+wilcox_results$adj_pvalue_BH <- p.adjust(wilcox_results$pvalue, method = "BH")
+wilcox_results <- wilcox_results %>%
+  rename(wilcox_pvalue = pvalue,
+         wilcox_adj_pvalue_Bonf = adj_pvalue_Bonf,
+         wilcox_adj_pvalue_BH = adj_pvalue_BH)
 
-tt_results_all <- t.test(homoplasy_mutations$norm_RoHO, mu = 0)$p.value
-
-
+save(tt_results, file = "tt_results.rda")
+save(wilcox_results, file = "wilcox_results.rda")
