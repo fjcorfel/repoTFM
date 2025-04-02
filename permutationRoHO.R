1a4f5c24ec4eec6cb21cd2e8f897a811236da38d
fjcorfel <fjcorfel@gmail.com>
2025-04-02 20:04:08 +0200

    Add permutation test for RoHO analysis

---
+++ permutationRoHO.R
@@ -0,0 +1,78 @@
+library(dplyr)
+library(pbmcapply)
+library(data.table)
+
+
+N_TOP_GENES <- 100
+N_PERMUTATIONS <- 1000
+
+# Load data dinamically
+load_rda_file <- function(file_path) {
+  load(file_path)
+  return(global_RoHO)
+}
+
+# List of .rda files
+rda_files <- c("../data/global/global_RoHO_homoplasies_agefilter40.rda")
+
+for (rda_file in rda_files) {
+  message(paste("Processing file:", rda_file))
+   
+  global_RoHO <- load_rda_file(rda_file)
+  
+  global_RoHO <- global_RoHO %>% arrange(desc(RoHO))
+  
+  observed_top_genes <- global_RoHO[1:N_TOP_GENES, ]
+  
+  top_genes <- unique(observed_top_genes$Rv_number)
+  
+  
+  process_gene <- function(gene) {
+    message(paste0("Processing ", gene))
+    
+    observed_count <- sum(observed_top_genes$Rv_number == gene)
+    # observerd_count_normalized, valor para samplear redondeado
+    # Perform random permutations
+    random_count <- numeric(N_PERMUTATIONS)
+    
+    for (i in 1:N_PERMUTATIONS) {
+      permuted_df <- global_RoHO %>% mutate(Rv_number = sample(Rv_number))
+      permuted_top_genes <- permuted_df[1:N_TOP_GENES, ]
+      random_count[i] <- sum(permuted_top_genes$Rv_number == gene)
+    }
+    
+    # Compute pvalue
+    # mu debería ser entorno a 0 siempre...
+    mu <- mean(random_count)
+    sigma <- sd(random_count)
+    pvalue <- pnorm(observed_count, mean = mu, sd = sigma, lower.tail = FALSE)
+    
+    return(list(pvalue = pvalue, observed_count = observed_count))
+  }
+  
+  set.seed(777)
+  results <- pbmclapply(top_genes,
+                        process_gene,
+                        mc.cores = 12,
+                        mc.preschedule = FALSE,
+                        mc.set.seed = TRUE)
+  
+  final_results <- data.frame(
+    Rv_number = top_genes,
+    observed_top_count = sapply(results, function(x) x$observed_count),
+    pvalue = sapply(results, function(x) x$pvalue)
+  ) %>%
+    left_join(global_RoHO %>% select(Rv_number, synonym),
+              by="Rv_number") %>%
+    distinct() %>%
+    arrange(pvalue) %>%
+    mutate(adj_pvalue_BH = p.adjust(pvalue, method = "BH")) %>%
+    select(Rv_number, synonym, pvalue, adj_pvalue_BH, observed_top_count)
+  
+  output_file <- paste0("permutation_results",
+                        tools::file_path_sans_ext(basename(rda_file)),
+                        ".csv")
+  
+  fwrite(final_results, paste0("../data/global/permutation_results/"), output_file)
+}
+
